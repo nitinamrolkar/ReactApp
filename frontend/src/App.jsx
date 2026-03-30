4522de515a869709719b346f1c7e564a9ee01c6e
nitinamrolkar <nitinamrolkar@gmail.com>
2026-03-31 03:55:59 +0530

    31 Mar evenning

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
+import { useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Sidebar from './components/Sidebar';
 import Dashboard from './pages/Dashboard';
-function App()
+
+import { createRoot } from 'react-dom/client'
+
+  function App()
 {
+
   return(
     
         <div className="row">
           <div className="col">
-
-                
            
                   <Header />
 
@@ -27,6 +29,8 @@ function App()
                 
                     <div style={{padding: "20px", width: "100%"}}>
                       <Dashboard />
+                      
+                      <NewForm  />
                     </div>
 
                   </div>
@@ -38,6 +42,31 @@ function App()
    
  
 
+   
   );
 }
+
+
+function NewForm(){
+
+  const [text, setText] = useState("");
+
+  function nitin(e){
+    setText(e.target.value);
+  }
+
+  function actionFun(e){
+     e.preventDefault();
+      alert(text); 
+  }
+
+  return (
+    <form onSubmit={actionFun}>
+      <label htmlFor=""> type your text</label>
+      <textarea value={text} onChange={nitin}></textarea>
+      <input type="submit" name="" id="" />
+    </form>
+  );
+}
+
 export default App;
